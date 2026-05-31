9f40f797c87b045203c39c7541e62f6efc4d6a2c
Yadi Cahyadi <dee@yadiCahyadi.local>
2026-05-31 18:57:53 +0700

    add model migration for ProductCategory

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+#[Fillable(['image', 'name', 'description'])]
+class ProductCategory extends Model
+{
+    //
+}
